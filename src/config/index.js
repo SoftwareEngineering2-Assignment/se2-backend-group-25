93300eb4f96c86b8f4e70090e93975840bccb595
george-21 <42096861+george-21@users.noreply.github.com>
2023-02-21 14:14:20 +0200

    Add comments to the code

--- src/config/index.js
+++ src/config/index.js
@@ -1,3 +1,7 @@
+// This file exports a single object with the mongoose instance for the entire app.
+
+// Require the mongoose module
 const mongoose = require('./mongoose');
 
-module.exports = {mongoose}; 
+// Export the mongoose object as a property of the module.exports object
+module.exports = { mongoose };
