ad1bd8ae8eccdf9c1d47727505563f30c1a59c62
george-21 <42096861+george-21@users.noreply.github.com>
2023-02-21 14:25:35 +0200

    Comment the middleware module

--- src/middlewares/validation.js
+++ src/middlewares/validation.js
@@ -2,9 +2,15 @@ const {schemas: validationSchemas} = require('../utilities/validation');
 
 module.exports = async (req, res, next, schema) => {
   /**
-     * @name validation
-     * @description Middleware that tests the validity of a body given a specified schema
-     */
+   * @name validation
+   * @description Middleware that tests the validity of a body given a specified schema
+   * @param {Object} req - The request object.
+   * @param {Object} res - The response object.
+   * @param {function} next - The next function in the middleware chain.
+   * @param {string} schema - The name of the schema to use for validation.
+   * @throws {Object} - Error object containing status and message properties.
+   */
+
   try {
     const {body} = req;
     await validationSchemas[schema].validate(body);
