c95849168a884e4808ffd1c96bc3cc246fc2f13c
george-21 <42096861+george-21@users.noreply.github.com>
2023-02-22 20:17:31 +0200

    Update index.js

--- src/utilities/validation/index.js
+++ src/utilities/validation/index.js
@@ -1,6 +1,8 @@
+// Importing the 'constants' and 'schemas' modules from their respective files
 const constants = require('./constants');
 const schemas = require('./schemas');
 
+// Exporting an object containing the 'constants' and 'schemas' modules
 module.exports = {
   constants,
   schemas
