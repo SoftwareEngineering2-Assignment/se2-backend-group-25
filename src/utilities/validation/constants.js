f4f51b06b15c9e3fd899e2082c3909d767c7bbe1
george-21 <42096861+george-21@users.noreply.github.com>
2023-02-22 20:16:48 +0200

    Update constants.js

--- src/utilities/validation/constants.js
+++ src/utilities/validation/constants.js
@@ -1,4 +1,7 @@
-const min = 5;
-const expires = '12h';
+const min = 5; // minimum length for passwords
+const expires = '12h'; // time period after which tokens expire
 
-module.exports = {min, expires};
+module.exports = {
+  min, // exporting the minimum length constant as part of the module object
+  expires // exporting the expiration time constant as part of the module object
+};
