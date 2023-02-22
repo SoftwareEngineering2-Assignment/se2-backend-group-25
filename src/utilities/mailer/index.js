8523f473d732670a8919abe9ca48fcfd64c33ced
george-21 <42096861+george-21@users.noreply.github.com>
2023-02-22 20:07:37 +0200

    Comments

--- src/utilities/mailer/index.js
+++ src/utilities/mailer/index.js
@@ -1,7 +1,13 @@
+// Importing the password module
 const password = require('./password');
+
+// Importing the send module
 const send = require('./send');
 
+// Exporting an object with mail and send properties
 module.exports = {
+  // Exports the password module as mail property
   mail: password,
+  // Exports the send module as send property
   send
 };
