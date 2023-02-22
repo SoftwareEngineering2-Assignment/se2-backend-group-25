40efb31968509b6f82660991c72fe76a9a853c1b
george-21 <42096861+george-21@users.noreply.github.com>
2023-02-22 20:14:58 +0200

    Comment the new version

--- src/utilities/mailer/send.js
+++ src/utilities/mailer/send.js
@@ -1,12 +1,16 @@
+// import SendGrid mail client library and set API key
 const sg = require('@sendgrid/mail');
-
 sg.setApiKey(process.env.SENDGRID_API_KEY);
 
+// export a function that sends an email to a user
 module.exports = (to, subject, email) => {
+
   /**
    * @name sendEmail
    * @description Is used to send an email to a user
    */
+
+  // create an object to specify the email's sender, recipient, subject, and HTML content
   const msg = {
     from: "karanikio@auth.gr",
     to,
@@ -14,6 +18,11 @@ module.exports = (to, subject, email) => {
     html: email
   };
 
+  // log the message for debugging purposes
   console.log(msg);
-  sg.send(msg).then(() => console.log("HERE")).catch(err=>console.log(err));
+
+  // send the message using the SendGrid client library
+  sg.send(msg)
+    .then(() => console.log("HERE"))
+    .catch(err => console.log(err));
 };
